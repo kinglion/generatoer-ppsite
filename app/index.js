1220266f1771d9fcdf41d8fa52a4089d9e362db6
kinglion <sangerenba@gmail.com>
2015-01-05 19:43:13 +0800

    配置generator文件

---
+++ app/index.js
@@ -0,0 +1,91 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+var BigGulpGenerator = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      '欢迎使用pp-web生成器!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'runNpmInstall',
+      message: '你要安装默认的gulp插件么?',
+      default: true
+    }, {
+      type: 'confirm',
+      name: 'createFolders',
+      message: '你要生成默认的目录结构么?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.runNpmInstall = props.runNpmInstall;
+      this.createFolders = props.createFolders;
+
+      if (this.runNpmInstall) {
+        var installDone = this.async();
+        this.npmInstall([
+          'gulp',
+          'gulp-autoprefixer',
+          'gulp-concat',
+          'gulp-csslint',
+          'gulp-jshint',
+          'gulp-minify-css',
+          'gulp-rename',
+          'gulp-sass',
+          'gulp-uglify',
+          'gulp-size'
+        ], { 'saveDev': true }, installDone);
+      }
+
+      if (this.createFolders) {
+        this.mkdir('scss');
+        this.mkdir('js');
+        this.mkdir('css');
+        this.mkdir('dist');
+        this.mkdir('dist/css');
+        this.mkdir('dist/js');
+
+        this.copy('.gitkeep', 'scss/.gitkeep');
+        this.copy('.gitkeep', 'css/.gitkeep');
+        this.copy('.gitkeep', 'js/.gitkeep');
+        this.copy('.gitkeep', 'dist/css/.gitkeep');
+        this.copy('.gitkeep', 'dist/js/.gitkeep');
+
+      }
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.copy('_package.json', 'package.json');
+      this.copy('_bower.json', 'bower.json');
+      this.copy('_base.scss', 'scss/base.scss');
+      this.copy('_base.css', 'css/base.css');
+      this.copy('_index.html', 'index.html');
+    },
+
+    projectfiles: function () {
+      this.copy('gulpfile.js', 'gulpfile.js');
+    }
+  },
+
+  end: function () {
+    this.installDependencies();
+  }
+});
+
+module.exports = BigGulpGenerator;
---
+++ app/templates/_gulpfile.js
@@ -0,0 +1,64 @@
+var gulp    = require('gulp'),
+    sass    = require('gulp-sass'),
+    rename  = require('gulp-rename'),
+    cssmin  = require('gulp-minify-css'),
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify'),
+    jshint  = require('gulp-jshint'),
+    csslint = require('gulp-csslint'),
+    prefix  = require('gulp-autoprefixer'),
+    size    = require('gulp-size');
+
+gulp.task('scss', function() {
+  return gulp.src('scss/all.scss')
+    .pipe(sass())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(prefix())
+    .pipe(rename('c.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('devscss',function(){
+	return gulp.src('scss/*.scss')
+		.pipe(sass())
+		.pipe(size({ gzip: true, showFiles: true }))
+		.pipe(prefix())
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('csslint', function() {
+  gulp.src('css/*.css')
+    .pipe(csslint({
+      'compatible-vendor-prefixes': false,
+      'box-sizing': false,
+      'important': false,
+      'known-properties': false
+    }))
+    .pipe(csslint.reporter());
+});
+
+gulp.task('js', function() {
+  gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(concat('j.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('jshint', function() {
+  gulp.src('dist/js/j.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('scss/*.scss', ['scss', 'csslint']);
+  gulp.watch('js/*.js', ['jshint', 'js']);
+});
+
+gulp.task('default', ['devscss', 'csslint', 'jshint', 'watch']);
+gulp.task('build', ['scss', 'csslint', 'js', 'jshint', 'watch']);
